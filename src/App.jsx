9474dfdb40fab04b04a2a5b94310d60b5ac88327
unapapa10 <fonseca34689@gmail.com>
2024-05-27 20:25:07 -0600

    feat:(front) layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@
 import { DatePicker, Button, ConfigProvider } from 'antd';
 import LayoutComponent from './components/Layout/index.jsx';
 import './App.css'
-
+import FormLogin from './components/FormLogin/index.jsx';
+import ImageLogin from './components/ImageLogin/imginicio.jsx'
 function App() {
   return (
     <ConfigProvider
@@ -14,7 +15,12 @@ function App() {
         }
       }}
       >
-      <LayoutComponent></LayoutComponent>
+      <LayoutComponent
+      leftColSize={{xs:0, sm:0, md:8, lg: 6}}
+      rightColSize={{xs:24, sm:24, md:16, lg:18}}
+      leftContent={<ImageLogin />}
+      rightContent={<FormLogin />}
+      />
     </ConfigProvider>
     /**<>
     <DatePicker />;
