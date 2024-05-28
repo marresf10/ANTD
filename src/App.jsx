c1f3f7dc0180ffbdb6e4145ee0c8574e1923c2e8
unapapa10 <fonseca34689@gmail.com>
2024-05-27 21:02:30 -0600

    feat:(back) routes creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import { DatePicker, Button, ConfigProvider } from 'antd';
-import LayoutComponent from './components/Layout/index.jsx';
+import { ConfigProvider } from 'antd';
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes';
 import './App.css'
-import FormLogin from './components/FormLogin/index.jsx';
-import ImageLogin from './components/ImageLogin/imginicio.jsx'
+
 function App() {
   return (
     <ConfigProvider
@@ -15,12 +15,9 @@ function App() {
         }
       }}
       >
-      <LayoutComponent
-      leftColSize={{xs:0, sm:0, md:8, lg: 6}}
-      rightColSize={{xs:24, sm:24, md:16, lg:18}}
-      leftContent={<ImageLogin />}
-      rightContent={<FormLogin />}
-      />
+    <BrowserRouter>
+    < AppRoutes/>
+    </BrowserRouter>
     </ConfigProvider>
     /**<>
     <DatePicker />;
