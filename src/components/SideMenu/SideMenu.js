5dfc44f2f3771ef83e1faeaedc14e7a35de64521
unapapa10 <fonseca34689@gmail.com>
2024-06-16 15:39:32 -0600

    merge de perfil con marlene

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,23 +1,28 @@
+// SideMenu.js
 import React from 'react';
 import { useAuth } from '../../hooks/useAuth';
-import './SideMenu.css';
+import FormProfile from '../FormProfile';
+import { useNavigate } from 'react-router-dom';
 
 const SideMenu = ({ onClose }) => {
     const { logout } = useAuth();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         logout();
     };
 
+    const handleEditProfile = () => {
+        navigate('/profile');  // Navega a la ruta de perfil
+    };
+
     return (
-        <div className="side-menu-container">
-            <div className="side-menu">
-                <button className="close-button" onClick={onClose}>Cerrar menú</button>
-                <button onClick={handleLogout}>Cerrar sesión</button>
-                <button>Editar perfil</button>
-            </div>
+        <div className="side-menu">
+            <button onClick={handleLogout}>Cerrar sesión</button>
+            <button onClick={handleEditProfile}>Editar perfil</button>
         </div>
     );
 };
 
 export default SideMenu;
+
